5cca6d3ce6281e4b3786b5eda3e469ac4574876c
elBeato <beatus.furrer@gmail.com>
2025-03-27 14:53:48 +0100

    register-step1 page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <div className="row-start-3 flex gap-40 flex-wrap items-center justify-center">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="./register"
+            href="./register-step1"
             rel="noopener noreferrer"
           >
             <Image
