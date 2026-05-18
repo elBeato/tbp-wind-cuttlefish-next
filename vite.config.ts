0aa0295d043ae40646ec11d2345aa706a9423bcb
Beat Furrer <beatus.furrer@gmail.com>
2026-05-18 13:31:59 +0200

    Update FE to BE release
    - allow hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'fundamental-jemima-thebellapapayas-d5aa4f14.koyeb.app',
+      'windseeker-app.ch',
+      'localhost',
+    ]
   },
   build: {
     outDir: 'dist',
