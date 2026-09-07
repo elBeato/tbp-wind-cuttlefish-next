a2b1d8fc4c13007bd4612d7f2f1e05b5cdeec539
Beat Furrer <beatus.furrer@gmail.com>
2026-09-07 09:47:12 +0200

    bugfix:

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ export default function App() {
         <Route path="/register-step2" element={<RegisterStep2 />} />
         <Route path="/register-step3" element={<RegisterStep3 />} />
         <Route path="/register-step4" element={<RegisterStep4 />} />
+        <Route path="/unsubscribe" element={<Unsubscribe />} />
         <Route path="/unsubscribe/:token" element={<Unsubscribe />} />
       </Route>
     </Routes>
